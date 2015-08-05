bf51a8a1382ed60cbff6e479664b50237b097466
Tunji Ruwase <tjruwase@gmail.com>
2015-08-05 02:15:11 -0700

    Linux specific code

---
+++ code/CanonicalCompute/LinuxUtils.cpp
@@ -0,0 +1,20 @@
+#include "Utils.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void SetTrainingThreadAffinity(int threadNum)
+{
+
+}
+
+void SetCanonicalConfig(int argc, char* argv[], CanonicalConfig& config)
+{
+
+}
+
+void DoModelCompute(int numThreads, ThreadLayerState *tl, DNNPass dp)
+{
+
+}
