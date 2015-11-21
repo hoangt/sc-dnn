bdc2708bd228b5e5ffb6e6398b781729e512b9a8
Tunji Ruwase <tjruwase@gmail.com>
2015-11-20 16:03:59 -0800

    IMG1K scripts

---
+++ code/SparseCompute/run_ZL3_IMG1K.sh
@@ -0,0 +1,13 @@
+nohup ./ZL3_ruby_IMG1K.sh 1 1 1 > nohup-ZL3_IMG1K_1_1_1.txt & 
+nohup ./ZL3_ruby_IMG1K.sh 1 1 2 > nohup-ZL3_IMG1K_1_1_2.txt & 
+nohup ./ZL3_ruby_IMG1K.sh 1 1 4 > nohup-ZL3_IMG1K_1_1_4.txt & 
+
+
+nohup ./ZL3_ruby_IMG1K.sh 2 2 1 > nohup-ZL3_IMG1K_2_2_1.txt & 
+nohup ./ZL3_ruby_IMG1K.sh 4 4 1 > nohup-ZL3_IMG1K_4_4_1.txt & 
+nohup ./ZL3_ruby_IMG1K.sh 8 8 1 > nohup-ZL3_IMG1K_8_8_1.txt & 
+
+
+ 
+
+
