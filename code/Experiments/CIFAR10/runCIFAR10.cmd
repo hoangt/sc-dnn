185e5905df8e56ca587f5578ed3b2ec72f514c62
unknown <olruwase@UPTONPARK-WIN10.redmond.corp.microsoft.com>
2015-11-25 09:14:49 -0800

    Add thread model option

--- code/Experiments/CIFAR10/runCIFAR10.cmd
+++ code/Experiments/CIFAR10/runCIFAR10.cmd
@@ -9,16 +9,17 @@ if "%1" == "" (
 
 @echo Running with ThreadOpt= %threadOpt%
 
-set STD_OPT=-affinity -startlayer:0 -zerosignalopt %threadOpt%
+set STD_OPT=-affinity -startlayer:0 -zerosignalopt %threadOpt% -threadmodel:2
 
-set SAMPLES=10000
+set SAMPLES=1000
 set FP=24
+set ACLS=8
 set BP=84
 set SCLS=83
 set WU=89
 set DCLS=67
 set MODEL=CIFAR10
-set sparseOpt=-forwardsparsity:%FP%  -backwardsparsity:%BP%  -signalcachelinesparsity:%SCLS% -weightupdatesparsity:%WU% -deltacachelinesparsity:%DCLS%
+set sparseOpt=-forwardsparsity:%FP%  -backwardsparsity:%BP%  -signalcachelinesparsity:%SCLS% -weightupdatesparsity:%WU% -deltacachelinesparsity:%DCLS% -activationcachelinesparsity:%ACLS%
 for /l %%i in (1,1,4) do (
  .\SparseCompute.exe %STD_OPT% -samples:%SAMPLES% -model:%MODEL% -kernel:%%i %sparseOpt%
 )
