0bd7519ac3c60f05f5c57b2fec47ee796abd9c2c
Tunji Ruwase <tjruwase@gmail.com>
2015-11-26 23:55:38 -0800

    Cache sweep experiments

---
+++ code/SparseCompute/CacheSweep/ForwardProp/run_L3_CIFAR10.sh
@@ -0,0 +1,12 @@
+if [ $# -lt 1 ]; then
+ t1=1
+else
+ t1=$1
+fi
+
+for i in 1 2 4 5 8 10 20; do
+  echo ./L3_ruby_CIFAR10.sh ${t1} ${t1} 1 ${i} > nohup-L3_CIFAR10_T${t1}_S${t1}_C${i}.txt & 
+done
+ 
+
+
