7226fcd1909ec8a1f827501d19c7e31fe4577a0e
Tunji Ruwase <tjruwase@gmail.com>
2015-11-27 01:26:16 -0800

    Fix naming bug

--- code/SparseCompute/CacheSweep/ForwardProp/run_ZL3_CIFAR10.sh
+++ code/SparseCompute/CacheSweep/ForwardProp/run_ZL3_CIFAR10.sh
@@ -7,7 +7,7 @@ fi
 commonDir="../Common"
 pass="ForwardProp"
 for i in 1 2 4 5 8 10 ; do
-  . ${commonDir}/L3_ruby_CIFAR10.sh ${t1} ${t1} 1 ${i} > nohup-L3_CIFAR10_T${t1}_S${t1}_C${i}.txt & 
+  . ${commonDir}/ZL3_ruby_CIFAR10.sh ${t1} ${t1} 1 ${i} > nohup-ZL3_CIFAR10_T${t1}_S${t1}_C${i}.txt & 
 done
  
 
