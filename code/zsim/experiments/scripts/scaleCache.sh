e4b2dadfac9f5a8a713ca85d7a4f6e154d2c6902
Tunji Ruwase <tjruwase@gmail.com>
2015-12-09 05:17:22 -0800

    Cache scaling script

---
+++ code/zsim/experiments/scripts/scaleCache.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+experimentDir=/home/tunji/sc-dnn/code/zsim/experiments
+scriptDir=${experimentDir}/scripts
+threadScript=${scriptDir}/scaleThreads.sh 
+
+if [ $# -ne 2 ]; then 
+	echo "Usage: $0 <machine> <benchmark>"
+	exit
+fi
+
+machineName=$1
+benchmarkName=$2
+nohupDir=nohupOut
+mkdir -p ${nohupDir}
+outputFile=${nohupDir}/${machineName}_${benchmarkName}.txt
+
+for i in 1 2 4 8; do 
+  nohup ${threadScript} ${machineName} ${benchmarkName} $i > ${outputFile} & 
+done
+
+
