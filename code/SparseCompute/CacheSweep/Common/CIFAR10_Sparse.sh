47e5b087a5f913e4c4078ecafdcb4299c6ed3768
Tunji <tjruwase@gmail.com>
2015-12-12 10:23:52 -0800

    Cleanup

--- code/SparseCompute/CacheSweep/Common/CIFAR10_Sparse.sh
+++
@@ -1,8 +0,0 @@
-model="CIFAR10"
-
-sparsityOpts="--forwardsparsity 100 --activationcachelinesparsity 8 --backwardsparsity 84 --signalcachelinesparsity 83 --weightupdatesparsity 89 --deltacachelinesparsity 67"
-
-modelOpts="$sparsityOpts --model ${model}"
-
-echo "MODELOPTS="${modelOpts}
-
